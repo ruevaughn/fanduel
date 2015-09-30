240c21360e373d4ba805e0ec60d9a875121e1e91
Chase Jensen <chase@xtiri.com>
2015-09-30 14:29:53 -0600

    start setting team

--- find-team.rb
+++ find-team.rb
@@ -20,6 +20,7 @@ class Team
 end
 
 class PlayerList
+  attr_accessor :id
   attr_accessor :players
   attr_accessor :quarterbacks
   attr_accessor :running_backs
@@ -31,25 +32,91 @@ class PlayerList
     @players = players
   end
 
+  # QBS
+
   def quarterbacks
-    # Use search so its objects not array
-    @players.map {|player| player if player.position == 'QB'}.compact
+    @quarterbacks ||= @players.map {|player| player if player.position == 'QB'}.compact
   end
+
+  def sorted_quarterbacks
+    @sorted_quarterbacks ||= quarterbacks.map {|qb| qb if qb.price_per_point != 0 }.compact
+    @sorted_quarterbacks.sort { |a,b| a.price_per_point <=> b.price_per_point}
+  end
+
+  def best_qb
+    sorted_quarterbacks.first
+  end
+
+  #RBS
 
   def running_backs 
     @players.map {|player| player if player.position == 'RB'}.compact
   end
 
+  def sorted_running_backs
+    @sorted_running_backs ||= running_backs.map {|player| player if player.price_per_point != 0 }.compact
+    @sorted_running_backs.sort { |a,b| a.price_per_point <=> b.price_per_point}
+  end
+
+  def best_rb1
+    sorted_running_backs[0]
+  end
+
+  def best_rb2
+    sorted_running_backs[1]
+  end
+
+  # WRS
+
   def wide_receivers
     @players.map {|player| player if player.position == 'WR'}.compact
   end
+
+  def sorted_wide_receivers
+    @sorted_wide_receivers ||= wide_receivers.map {|player| player if player.price_per_point != 0 }.compact
+    @sorted_wide_receivers.sort { |a,b| a.price_per_point <=> b.price_per_point}
+  end
+
+  def best_wr1
+    sorted_wide_receivers[0]
+  end
+
+  def best_wr2
+    sorted_wide_receivers[1]
+  end
+
+  def best_wr3
+    sorted_wide_receivers[2]
+  end
+
+  # TE
 
   def tight_ends
     @players.map {|player| player if player.position == 'TE'}.compact
   end 
 
+  def sorted_tight_ends
+    @sorted_tight_ends ||= tight_ends.map {|player| player if player.price_per_point != 0 }.compact
+    @sorted_tight_ends.sort { |a,b| a.price_per_point <=> b.price_per_point}
+  end
+
+  def best_te
+    sorted_tight_ends[0]
+  end
+
+  # K
+
   def kickers
     @players.map {|player| player if player.position == 'K'}.compact
+  end
+
+  def sorted_kickers
+    @sorted_kickers ||= kickers.map {|player| player if player.price_per_point != 0 }.compact
+    @sorted_kickers.sort { |a,b| a.price_per_point <=> b.price_per_point}
+  end
+
+  def best_k
+    sorted_kickers[0]
   end
 
 end
@@ -76,7 +143,7 @@ class Player
 
   def price_per_point 
     if self.salary.nonzero? && self.fppg.nonzero?
-      self.salary / self.fppg  
+      (self.salary.to_f / self.fppg.to_f).round(2)
     else
       0.0 
     end
@@ -85,9 +152,12 @@ class Player
 end
 
 players = []
+count = 0
 
 CSV.foreach("./FanDuel-NFL-2015-10-01-13125-players-list.csv") do |row|
-  id = row[0].to_i
+  count += 1  
+  next if count == 1 
+  id = count - 1
   position = row[1]
   name   = "#{row[2]} #{row[3]}"   
   fppg   = row[4].to_i
@@ -111,5 +181,25 @@ end
 team = Team.new
 list = PlayerList.new(players)
 
-player = players.first
-puts "#{player.name} - #{player.price_per_point}"
+team.qb = list.best_qb
+team.rb1 = list.best_rb1
+team.rb2 = list.best_rb2
+team.wr1 = list.best_wr1
+team.wr2 = list.best_wr2
+team.wr3 = list.best_wr3
+team.te =  list.best_te
+team.k = list.best_k
+
+puts team.qb.name
+puts team.rb1.name
+puts team.rb2.name
+puts team.wr1.name
+puts team.wr2.name
+puts team.wr3.name
+puts team.te.name
+puts team.k.name
+
+
+
+
+
